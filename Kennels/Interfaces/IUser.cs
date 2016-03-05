2a48e67fedb11beabbfeaac847736300d6385375
Barry O'Kane <arcangel666@hotmail.co.uk>
2016-03-05 21:06:49 +0000

    User Interfasce and ViewModel completed.

---
+++ Kennels/Interfaces/IUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kennels.Interfaces
+{
+    interface IUser
+    {
+        User AddUser(string name, string password, User createdBy, DateTime createdOn, bool isAdmin);
+
+        bool DeleteUser(User user);
+
+        void ToggleUserAdmin(User adminUser, User user);
+
+        void ChangeUserPassword(User user, string newPassword);
+
+        List<User> GetAllUsers();
+
+        User GetUserByName(string name);
+    }
+}
---
+++ Kennels/ViewModels/UserViewModel.cs
@@ -0,0 +1,147 @@
+﻿using Kennels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kennels.ViewModels
+{
+    class UserViewModel : IUser
+    {
+        private KennelsModelContainer _context;
+
+        public UserViewModel(KennelsModelContainer context)
+        {
+            _context = context;
+        }
+
+        public User AddUser(string name, string password, User createdBy, DateTime createdOn, bool isAdmin)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("New User name not specified.");
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentException("New User password not specified.");
+
+            if (createdBy == null)
+                throw new ArgumentException("New User Created By User not specified.");
+
+            if (createdOn == null)
+                throw new ArgumentException("New User Created date not specified.");
+
+            User u = new User
+            {
+                Name = name,
+                Password = password,
+                IsObsolete = false,
+                CreatedBy = createdBy.Name,
+                CreatedDate = createdOn,
+                IsAdmin = isAdmin
+            };
+
+            try
+            {
+                _context.Users.Add(u);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            return u;
+        }
+
+        public void ChangeUserPassword(User user, string newPassword)
+        {
+            if (user == null)
+                throw new ArgumentException("User to have password updated not specified.");
+
+            if (string.IsNullOrEmpty(newPassword))
+                throw new ArgumentException("New Password not specified.");
+
+            if (newPassword.Equals(user.Password))
+                throw new ArgumentException("New and Old Passwords are the same.");
+
+            try
+            {
+                User usr = _context.Users.Where(u => u.UserID == user.UserID).FirstOrDefault();
+
+                usr.Password = newPassword;
+
+                _context.SaveChanges();
+            }
+            catch (Exception) { }
+        }
+
+        public bool DeleteUser(User user)
+        {
+            if (user == null)
+                throw new ArgumentException("User to be deleted not specified.");
+
+            try
+            {
+                User usr = _context.Users.Where(u => u.UserID == user.UserID).FirstOrDefault();
+
+                usr.IsObsolete = true;
+
+                _context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<User> GetAllUsers()
+        {
+            try
+            {
+                return _context.Users.ToList();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public User GetUserByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("User name not specified.");
+
+            try
+            {
+                return _context.Users.Where(u => u.Name.Equals(name)).FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public void ToggleUserAdmin(User adminUser, User user)
+        {
+            if (adminUser == null)
+                throw new ArgumentException("Admin User not specified.");
+
+            if (!adminUser.IsAdmin)
+                throw new ArgumentException("Admin User specified does not have permission.");
+
+            if (user == null)
+                throw new ArgumentException("User to be mande Admin not specified.");
+
+            try
+            {
+                User usr = _context.Users.Where(u => u.UserID == user.UserID).FirstOrDefault();
+
+                usr.IsAdmin = true;
+
+                _context.SaveChanges();
+            }
+            catch (Exception) { }
+        }
+    }
+}
