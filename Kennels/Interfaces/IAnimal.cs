f8e42655206c676a652a44233473638669b82b65
Barry O'Kane <arcangel666@hotmail.co.uk>
2016-03-06 15:21:25 +0000

    Animal Interface and ViewModel implemented.

---
+++ Kennels/Interfaces/IAnimal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kennels.Interfaces
+{
+    interface IAnimal
+    {
+        List<Animal> GetAllAnimals();
+
+        List<Animal> GetAllActiveAnimals();
+
+        List<Animal> GetAllDeletedAnimals();
+    }
+}
---
+++ Kennels/ViewModels/AnimalViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kennels.Interfaces;
+
+namespace Kennels.ViewModels
+{
+    class AnimalViewModel : IAnimal
+    {
+        KennelsModelContainer _context;
+
+        public AnimalViewModel(KennelsModelContainer context)
+        {
+            _context = context;
+        }
+
+        public List<Animal> GetAllActiveAnimals()
+        {
+            try
+            {
+                return _context.Animals.Where(a => a.IsObsolete == false).ToList();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public List<Animal> GetAllAnimals()
+        {
+            try
+            {
+                return _context.Animals.ToList();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public List<Animal> GetAllDeletedAnimals()
+        {
+            try
+            {
+                return _context.Animals.Where(a => a.IsObsolete == true).ToList();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
